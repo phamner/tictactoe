7b1c8a71c344f0a0e755a623d80099c6a0d39eb2
Philip Hamner <philiphamner@gmail.com>
2019-10-19 12:11:31 -0700

    first commit, TTT

---
+++ script.js
@@ -0,0 +1,37 @@
+var origBoard;  //will eventually be an array that tracks the board
+const humanPlayer = 'O';
+const compPlayer = 'X';
+const winCombos = [
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8],
+	[0,4,8],
+	[6,4,2]
+];
+
+const cells = document.querySelectorAll('.cell');
+
+startGame();
+
+function startGame(){
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for(var i = 0; i < cells.length; i++){
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false)
+	}
+}
+
+function turnClick(square){
+	// console.log(square.target.id)
+	turn(square.target.id, humanPlayer)
+}
+
+function turn(squareId, player){
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+}
