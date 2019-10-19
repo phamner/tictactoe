b7e1e1c09c77d9f0457b7993b28aa771f696a68b
Philip Hamner <philiphamner@gmail.com>
2019-10-19 16:06:15 -0700

    working tic tac toe, but with dumb AI

--- script.js
+++ script.js
@@ -27,17 +27,63 @@ function startGame(){
 }
 
 function turnClick(square){
-	// console.log(square.target.id)
-	turn(square.target.id, humanPlayer)
+	if(typeof origBoard[square.target.id] == 'number'){
+		turn(square.target.id, humanPlayer)
+		if(!checkTie()) turn(bestSpot(), compPlayer);
+	}
 }
 
 function turn(squareId, player){
 	origBoard[squareId] = player;
 	document.getElementById(squareId).innerText = player;
 	let gameWon = checkWin(origBoard, player);
-	if(gameWon){
-		gameOver(gameWon)
-	}
+	if(gameWon) gameOver(gameWon)
 }
 
-function checkWin(board, player){}
+function checkWin(board, player){
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()){
+		if(win.every(elem => plays.indexOf(elem) > -1)){
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver (gameWon){
+	for(let index of winCombos[gameWon.index]){
+		document.getElementById(index).style.backgroundColor = gameWon.player == humanPlayer ? "blue" : "red";
+	}
+	for (var i = 0; i < cells.length; i++){
+		cells[i].removeEventListener('click', turnClick, false)
+	}
+	declareWinner(gameWon.player == humanPlayer ? "You Win!" : "You Lose!")
+}
+
+function declareWinner(who){
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = who;
+
+}
+
+function emptySquares(){
+	return origBoard.filter(s => typeof s =='number')
+}
+
+function bestSpot (){
+	return emptySquares()[0]
+}
+
+function checkTie (){
+	if (emptySquares().length == 0){
+		for(var i = 0; i < cells.length; i++){
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game!")
+		return true;
+	}
+}
